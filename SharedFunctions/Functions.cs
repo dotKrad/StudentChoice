dc92d1158486e8552d7b011abacd011e4eb603e8
Yordan Suarez <ysantiago1985@gmail.com>
2022-11-20 21:08:51 -0500

    added functions class with shared implementation

---
+++ SharedFunctions/Functions.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.RegularExpressions;
+
+namespace SharedFunctions
+{
+    public static class Functions
+    {
+
+        public static string GetSmallestDifference(string filename, string pattern, int minGroup, int maxGroup, int returnGroup)
+        {
+            int spread;
+            string result = "";
+
+            int max;
+            int min;
+
+            int minDiff = int.MaxValue;
+
+            var regex = new Regex(pattern);
+
+            foreach (string line in File.ReadLines(filename))
+            {
+                var match = regex.Match(line);
+                if (match.Success)
+                {
+                    min = int.Parse(match.Groups[minGroup].ToString());
+                    max = int.Parse(match.Groups[maxGroup].ToString());
+                    spread = Math.Abs(max - min);
+
+
+                    //Console.WriteLine($"{match.Groups[1]} {min} {max} {spread}");
+
+                    if (spread < minDiff)
+                    {
+                        minDiff = spread;
+                        result = match.Groups[returnGroup].ToString();
+                    }
+                }
+
+
+            }
+
+            return result;
+        }
+    }
+}
