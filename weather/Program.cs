1e1ed54e622873fd799f2dba887474d89ea01e98
Yordan Suarez <ysantiago1985@gmail.com>
2022-11-20 21:10:12 -0500

    refactor weather project to use shared function
    after I read the last instruction I assumed the numbers in the weather data were integer

--- weather/Program.cs
+++ weather/Program.cs
@@ -1,48 +1,11 @@
 ﻿
-using System.Text.RegularExpressions;
+using SharedFunctions;
 
 const string filename = "weather.dat";
-int counter = 0;
 
-// Read the file and display it line by line.  
-double spread;
-int foundAt = 0;
-
-double max;
-double min;
-
-double minSpread = double.MaxValue;
-
-var regex = new Regex(@"^\s*(\d+)\*?\s*(\d+)\*?\s*(\d+).*");
+var pattern = @"^\s*(\d+)\*?\s*(\d+)\*?\s*(\d+).*";
 
 
+var result = Functions.GetSmallestDifference(filename, pattern, 3, 2, 1);
 
-
-foreach (string line in File.ReadLines(filename))
-{
-    counter++;
-    //skipping 1st and 2nd lines
-    if (counter <= 2)
-        continue;
-
-
-    var match = regex.Match(line);
-    if (match.Success)
-    {
-        min = double.Parse(match.Groups[3].ToString());
-        max = double.Parse(match.Groups[2].ToString());
-        spread = max - min;
-
-
-        //Console.WriteLine($"{match.Groups[1]} {min} {max} {spread}");
-
-        if (spread < minSpread)
-        {
-            minSpread = spread;
-            foundAt = int.Parse(match.Groups[1].ToString());
-        }
-    }
-
-
-}
-Console.WriteLine(foundAt);
+Console.WriteLine(result);
