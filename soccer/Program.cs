7f5251731c574fb9d7c4ce42e152fc494503fc66
Yordan Suarez <ysantiago1985@gmail.com>
2022-11-20 21:09:18 -0500

    refactor soccer project to use shared function

--- soccer/Program.cs
+++ soccer/Program.cs
@@ -1,44 +1,17 @@
-﻿using System.Text.RegularExpressions;
+﻿using SharedFunctions;
 
 const string filename = "football.dat";
 // Read the file and display it line by line.  
-int spread;
-string name = "";
 
-int max;
-int min;
-
-int minDiff = int.MaxValue;
 
 /*
  * 1 - name
  * 2 - F
  * 3 - A
  */
-var regex = new Regex(@"^\s*\d+\.\*?\s*([\w|_]+)\*?\s*[\w ]{20,20}(\d+)\s+\-\s+(\d+).*");
+const string pattern = @"^\s*\d+\.\*?\s*([\w|_]+)\*?\s*[\w ]{20,20}(\d+)\s+\-\s+(\d+).*";
+
+var result = Functions.GetSmallestDifference(filename, pattern, 3, 2, 1);
 
 
-
-
-foreach (string line in File.ReadLines(filename))
-{
-    var match = regex.Match(line);
-    if (match.Success)
-    {
-        min = int.Parse(match.Groups[3].ToString());
-        max = int.Parse(match.Groups[2].ToString());
-        spread = Math.Abs(max - min);
-
-
-        //Console.WriteLine($"{match.Groups[1]} {min} {max} {spread}");
-
-        if (spread < minDiff)
-        {
-            minDiff = spread;
-            name = match.Groups[1].ToString();
-        }
-    }
-
-
-}
-Console.WriteLine(name);
+Console.WriteLine(result);
