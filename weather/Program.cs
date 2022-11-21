7ca5cf775c5a6a122599b9946dc03576e06fa757
Yordan Suarez <ysantiago1985@gmail.com>
2022-11-20 20:34:21 -0500

    weather program

--- weather/Program.cs
+++ weather/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Text.RegularExpressions;
+
+const string filename = "weather.dat";
+int counter = 0;
+
+// Read the file and display it line by line.  
+double spread;
+int foundAt = 0;
+
+double max;
+double min;
+
+double minSpread = double.MaxValue;
+
+var regex = new Regex(@"^\s*(\d+)\*?\s*(\d+)\*?\s*(\d+).*");
+
+
+
+
+foreach (string line in File.ReadLines(filename))
+{
+    counter++;
+    //skipping 1st and 2nd lines
+    if (counter <= 2)
+        continue;
+
+
+    var match = regex.Match(line);
+    if (match.Success)
+    {
+        min = double.Parse(match.Groups[3].ToString());
+        max = double.Parse(match.Groups[2].ToString());
+        spread = max - min;
+
+
+        //Console.WriteLine($"{match.Groups[1]} {min} {max} {spread}");
+
+        if (spread < minSpread)
+        {
+            minSpread = spread;
+            foundAt = int.Parse(match.Groups[1].ToString());
+        }
+    }
+
+
+}
+Console.WriteLine(foundAt);
